e6ae163d83beb026b7cc202dd52f38d346b581cd
EpicCarlito <jameschef2018@gmail.com>
2024-03-29 14:25:42 -0400

    changed tsconfig.json

--- .build/index.js
+++ .build/index.js
@@ -22,119 +22,70 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var dns_1 = __importDefault(require("dns"));
-var dnsPromises = dns_1.default.promises;
-var net = __importStar(require("net"));
-var emailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-function checkEmail(email, domain) {
-    return __awaiter(this, void 0, void 0, function () {
-        var formatCheck, hostname, addresses, sortedAddresses, firstAddress_1, amountWritten_1, responseData_1, err_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    formatCheck = emailFormat.test(email);
-                    if (!formatCheck)
-                        return [2 /*return*/, false];
-                    hostname = email.split("@")[1];
-                    if (hostname != domain)
-                        return [2 /*return*/, false];
-                    _a.label = 1;
-                case 1:
-                    _a.trys.push([1, 3, , 4]);
-                    return [4 /*yield*/, dnsPromises.resolveMx(hostname)];
-                case 2:
-                    addresses = _a.sent();
-                    if (addresses && addresses.length > 0) {
-                        sortedAddresses = addresses.sort(function (a, b) { return a.priority - b.priority; });
-                        firstAddress_1 = sortedAddresses[0].exchange;
-                        amountWritten_1 = 0;
-                        responseData_1 = '';
-                        return [2 /*return*/, new Promise(function (resolve) {
-                                var connection = net.createConnection({ port: 25, host: firstAddress_1, timeout: 5000 });
-                                connection.on('data', function (data) {
-                                    responseData_1 += data.toString();
-                                    if (data.indexOf("220") === 0 || data.indexOf("250") === 0) {
-                                        if (amountWritten_1 < 2) {
-                                            connection.write("HELO ".concat(firstAddress_1, "\r\n"));
-                                            connection.write("MAIL FROM: <noreply@example.com>\r\n");
-                                            connection.write("RCPT TO: <".concat(email, ">\r\n"));
-                                            connection.write('\r\n');
-                                            amountWritten_1++;
-                                        }
-                                        else {
-                                            connection.end();
-                                        }
-                                    }
-                                    else {
-                                        connection.end();
-                                    }
-                                });
-                                connection.on('end', function () {
-                                    if (responseData_1.indexOf("550") != -1 || responseData_1.indexOf("502") != -1 || responseData_1.indexOf("500") != -1) {
-                                        resolve(false);
-                                    }
-                                    else if (responseData_1.indexOf("250") != -1 && responseData_1.indexOf("550") == -1 || responseData_1.indexOf("502") == -1 || responseData_1.indexOf("500") == -1) {
-                                        resolve(true);
-                                    }
-                                    else {
-                                        resolve(false);
-                                    }
-                                });
-                                connection.on('error', function () {
-                                    resolve(false);
-                                });
-                            })];
+const dns_1 = __importDefault(require("dns"));
+const dnsPromises = dns_1.default.promises;
+const net = __importStar(require("net"));
+const emailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+async function checkEmail(email, domain) {
+    const formatCheck = emailFormat.test(email);
+    if (!formatCheck)
+        return false;
+    const hostname = email.split("@")[1];
+    if (hostname != domain)
+        return false;
+    try {
+        const addresses = await dnsPromises.resolveMx(hostname);
+        if (addresses && addresses.length > 0) {
+            const sortedAddresses = addresses.sort((a, b) => a.priority - b.priority);
+            const firstAddress = sortedAddresses[0].exchange;
+            let amountWritten = 0;
+            let responseData = '';
+            return new Promise((resolve) => {
+                const connection = net.createConnection({ port: 25, host: firstAddress, timeout: 5000 });
+                connection.on('data', (data) => {
+                    responseData += data.toString();
+                    if (data.indexOf("220") === 0 || data.indexOf("250") === 0) {
+                        if (amountWritten < 2) {
+                            connection.write(`HELO ${firstAddress}\r\n`);
+                            connection.write(`MAIL FROM: <noreply@example.com>\r\n`);
+                            connection.write(`RCPT TO: <${email}>\r\n`);
+                            connection.write('\r\n');
+                            amountWritten++;
+                        }
+                        else {
+                            connection.end();
+                        }
                     }
                     else {
-                        return [2 /*return*/, false];
+                        connection.end();
                     }
-                    return [3 /*break*/, 4];
-                case 3:
-                    err_1 = _a.sent();
-                    return [2 /*return*/, false];
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
+                });
+                connection.on('end', () => {
+                    if (responseData.indexOf("550") != -1 || responseData.indexOf("502") != -1 || responseData.indexOf("500") != -1) {
+                        resolve(false);
+                    }
+                    else if (responseData.indexOf("250") != -1 && responseData.indexOf("550") == -1 || responseData.indexOf("502") == -1 || responseData.indexOf("500") == -1) {
+                        resolve(true);
+                    }
+                    else {
+                        resolve(false);
+                    }
+                });
+                connection.on('error', () => {
+                    resolve(false);
+                });
+            });
+        }
+        else {
+            return false;
+        }
+    }
+    catch (err) {
+        return false;
+    }
 }
 exports.default = checkEmail;
